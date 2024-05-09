9564842f226335064d6a1628d11ff2371bdec5ec
Ilyas Belkheiri <ebelkhei@e2r12p1.1337.ma>
2024-05-09 15:15:24 +0100

    added a handler for JSON responses

--- main.go
+++ main.go
@@ -1,21 +1,61 @@
 package main
 
 import (
-	"github.com/joho/godotenv"
+	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
 )
+
+type Test struct {
+	Val string
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
+	
 	mux := http.NewServeMux()
-
-	fmt.Printf("Listening on port %s ...", os.Getenv("PORT"))
+	mux.HandleFunc("/ids/*", getId)
+	
+	fmt.Println("Listening on port", os.Getenv("PORT"), "...")
 	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), mux))
+}
+
+func respondWithJSON(w http.ResponseWriter, status int, payload interface{}) {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	response, err := json.Marshal(payload)
+
+	if err != nil {
+		http.Error(w, "err marshaling", http.StatusInternalServerError)
+	}
+
+	w.Write([]byte(response))
+}
+
+func getId(w http.ResponseWriter, r *http.Request) {
+		uri, err := url.Parse(r.RequestURI)
+		
+		if err != nil {
+			fmt.Println("Err parsing uri")
+		}
+	
+		id, err := strconv.Atoi(uri.Path[len("/ids/"):])
+		if err != nil {
+			fmt.Println("err casting id to int")
+		}
+		fmt.Printf("id: -%d-", id)
+		if id > 10 {
+			respondWithJSON(w, 200, Test{"All good :)"}) 
+		} else {
+			respondWithJSON(w, 404, Test{"Not found :("})
+		}
 }
