aa695ea87da9effe3b11c9d46410d7f3cbbb6657
Elias-Belkheiri <ilyasseyasser3@gmail.com>
2024-07-21 16:11:27 +0100

    added a query to retrieve the feeds to fetch next

--- sql/queries/feed.sql
+++ sql/queries/feed.sql
@@ -3,3 +3,6 @@ INSERT INTO feeds (id, name, url, created_at, updated_at) VALUES ($1, $2, $3, $4
 
 -- name: GetFeeds :many
 SELECT * FROM feeds;
+
+-- name: GetNextFeedsToFetch :many
+SELECT * FROM feeds ORDER BY last_fetched_at NULL FIRST, last_fetched_at ASC;
